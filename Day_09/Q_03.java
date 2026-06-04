2e057940231a67ff322c45797b5bb1716e65e4f0
Yatharth Singh <singhyatharthx29@gmail.com>
2026-06-04 12:11:23 +0530

    added day 9

---
+++ Day_09/Q_03.java
@@ -0,0 +1,15 @@
+public class Q_03{
+    public static void main(String[] args) {
+        char k='A';
+        for( int i=1;i<=5;i++){
+            
+            for(int j=1;j<=i;j++){
+
+            System.out.print(k+" ");
+            }
+            k++;
+            System.out.println("");
+        }
+        
+    }
+}
